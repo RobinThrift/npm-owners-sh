b1c9e30493ed64321d01d174be145f93cee6d407
Robin Thrift <thrift.d.robin@googlemail.com>
2015-02-18 15:05:19 +0100

    fixed echo... again...

--- npm-owners.sh
+++ npm-owners.sh
@@ -3,12 +3,12 @@
 if [[ -r "$1" ]]; then
     contribs=$(cat $1);
     for i in "$contribs"; do
-        echo "Adding $1 as an owner...";
+        echo "Adding $i as an owner...";
         npm owner add "$i";
     done;
 else
     for i in "$@"; do
-        echo "Adding $1 as an owner...";
+        echo "Adding $i as an owner...";
         npm owner add "$i";
     done;
 fi;
